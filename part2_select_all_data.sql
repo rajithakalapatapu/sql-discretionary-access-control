d92627e60461a9f3eabd4d6fd6559ffda2795251
rajitha <rajithakalapatapu9@gmail.com>
2018-11-16 22:12:23 -0600

    q3 part 2

---
+++ part2_select_all_data.sql
@@ -0,0 +1,26 @@
+TEE part2_spool.txt;
+
+-- Part 3 question 2
+
+-- Account Privilege table data
+SELECT * FROM security_database.account_privilege;
+
+-- Assigned With table data
+SELECT * FROM security_database.assigned_with;
+
+-- Part of table data
+SELECT * FROM security_database.part_of;
+
+-- relation privilege table data
+SELECT * FROM security_database.relation_privilege;
+
+-- 'table' data
+SELECT * FROM security_database.`table`;
+
+-- user_account table data
+SELECT * FROM security_database.user_account;
+
+-- user_role table data
+SELECT * FROM security_database.user_role;
+
+NOTEE;
